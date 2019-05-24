29e69da59c3a71a9c6ae8b1b2b2ce65a72bc3741
Seres István András <seresistvanandras@Seress-MacBook-Air.local>
2019-05-24 09:39:05 +0200

    Bugs fixed spotted by Harry

--- contracts/MixerContract.sol
+++ contracts/MixerContract.sol
@@ -28,14 +28,17 @@ contract MixerContract {
     function payOutCleanCoins(address A, address[] memory P, uint8 v, bytes32 r, bytes32 s) public {
         require(sessions[A].timeOut > now, "Participants should have withdrawn clean coins earlier!");
         require(sessions[A].arrivedAmount != 0, "This mixing session is already over!");
-        require(sessions[A].arrivedAmount == P.length, "Not all participants deposited!");
+        require(sessions[A].senderAddresses.length == P.length, "Not all participants deposited!");
         require(ecrecover(keccak256(abi.encode(P)), v, r, s)==A, "Threshold sig is not valid"); //check whether the payout addresses are signed
+        uint totalAmt = sessions[A].arrivedAmount;
+        sessions[A].arrivedAmount = 0;
         for(uint i=0; i < P.length; i++) {
             address(uint160(P[i])).transfer(amt);
+            delete sessions[A].senderAddresses[i];
         }
-        msg.sender.transfer(sessions[A].arrivedAmount-P.length*amt); //any surplus is provided to the one who sends the tx
-        sessions[A].arrivedAmount = 0;
-
+        if (totalAmt-P.length*amt > 0) {
+            msg.sender.transfer(totalAmt-P.length*amt); //any surplus is provided to the one who sends the tx
+        }
     }
 
     //If threshold signing fails, participants can get back their coins when timeout expires
@@ -43,6 +46,6 @@ contract MixerContract {
         require(now > sessions[A].timeOut, "You cannot withdraw dirty coins before timeout expires!");
         require(sessions[A].senderAddresses[index] == msg.sender, "You are not entitled to receive dirty coins!");
         msg.sender.transfer(amt);
-        sessions[A].senderAddresses[index] = address(0);
+        delete sessions[A].senderAddresses[index]; // saving gas by erasing mapping items
     }
 }
